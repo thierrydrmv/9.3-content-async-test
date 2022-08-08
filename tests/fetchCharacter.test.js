3707ef599a9eb2b24fc86983e885f603de72c7c3
Thierry <varelathierry@gmail.com>
2022-08-08 16:14:04 -0300

    testes assincronos da funcao fetchCharacter

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -13,4 +13,14 @@ describe('Teste a função fetchCharacter', () => {
     console.log(failRequest);
     expect(failRequest).toEqual(new Error('You must provide an url'));
   })
+  it('Verifica se retorna \'Invalid id\' ao executar a função com parâmetro que não existe', async () => {
+    const response = await fetchCharacter('parâmetro qualquer');
+    expect(response).toBe('Invalid id');
+  });
+  it('Verifica se fetch é chamada com o endpoint correto', async () => {
+    const url = 'https://www.superheroapi.com/api.php/4192484924171229/720';
+    await fetchCharacter('720');
+    expect(fetch).toHaveBeenCalledTimes(4);
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
 });
